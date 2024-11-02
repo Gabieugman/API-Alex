ec87b005109fdf10eb9b931e632a64473157cfae
Victor Hugo <victorgugoteles2010@gmail.com>
2024-11-01 22:37:36 -0300

    TENTATIVA 4

--- routes/user.js
+++ routes/user.js
@@ -2,8 +2,8 @@ const express = require('express');
 const router = express.Router();
 const userController = require('../controller/userController');
 
-router.post('/', userController.create);
-router.get('/', userController.getAll);
+router.post('/criar', userController.create);
+router.get('/selecionarTodos', userController.getAll);
 router.put('/:id_pessoa', userController.getOne);
 router.put('/:id_pessoa', userController.update);
 router.delete('/:id_pessoa', userController.delete);
